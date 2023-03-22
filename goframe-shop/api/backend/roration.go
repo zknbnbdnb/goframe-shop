68fd1928c4cdd583dc8bb7f2c88efe6dd813e118
zknbnbdnb <zk1307403902@gmail.com>
2023-03-22 15:53:02 +0800

    学会了orm的链式操作和goframe的orm框架设计的思想，和字符段的过滤以及轮播图的跟新

--- goframe-shop/api/backend/roration.go
+++ goframe-shop/api/backend/roration.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 )
 
+// Create 创建
 type RotationReq struct {
 	g.Meta `path:"/backend/rotation/add" tags:"Rotation" method:"post" summary:"这是个注释"`
 	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
@@ -14,8 +15,19 @@ type RotationRes struct {
 	RotationId int `json:"rotationId"`
 }
 
+// Delete 删除
 type RotationDeleteReq struct {
 	g.Meta `path:"/backend/rotation/delete" method:"delete" tags:"轮播图" summary:"删除轮播图接口"`
 	Id     uint `v:"min:1#请选择需要删除的轮播图" dc:"轮播图id"`
 }
 type RotationDeleteRes struct{}
+
+// Update 更新
+type RotationUpdateReq struct {
+	g.Meta `path:"/backend/rotation/update/{Id}" method:"post" tags:"轮播图" summary:"修改轮播图接口"`
+	Id     uint   `json:"id"         v:"min:1#请选择需要修改的轮播图" dc:"轮播图Id"`
+	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"       v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort"       dc:"排序"` // 当非必需时，可以不写v:"required#排序不能为空"
+}
+type RotationUpdateRes struct{}
--- goframe-shop/internal/logic/rotation/rotation.go
+++ goframe-shop/internal/logic/rotation/rotation.go
@@ -20,6 +20,7 @@ func New() *sRotation {
 	return &sRotation{}
 }
 
+// Create 创建
 func (s *sRotation) Create(ctx context.Context, in model.RotationCreateInput) (out model.RotationCreateOutput, err error) {
 	// 不允许HTML代码
 	if err = ghtml.SpecialCharsMapOrStruct(in); err != nil {
@@ -39,6 +40,23 @@ func (s *sRotation) Delete(ctx context.Context, id uint) error {
 		_, err := dao.RotationInfo.Ctx(ctx).Where(g.Map{
 			dao.RotationInfo.Columns().Id: id,
 		}).Delete() // 这里是软删除，如果要实现硬删除可以在Delete()前面加上Unscoped()
+		return err
+	})
+}
+
+// Update 修改
+func (s *sRotation) Update(ctx context.Context, in model.RotationUpdateInput) error {
+	return dao.RotationInfo.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error {
+		// 不允许HTML代码
+		if err := ghtml.SpecialCharsMapOrStruct(in); err != nil {
+			return err
+		}
+		_, err := dao.RotationInfo.
+			Ctx(ctx).
+			Data(in).
+			FieldsEx(dao.RotationInfo.Columns().Id).
+			Where(dao.RotationInfo.Columns().Id, in.Id).
+			Update()
 		return err
 	})
 }
